8242ae9f0f4927c0ebd4fd25c4d6c7b7d3266274
Stanciu Dragoș Ioan <39739297+StanciuDragosIoan@users.noreply.github.com>
2019-11-21 11:21:30 +0200

    test

---
+++ notes.js
@@ -0,0 +1,20 @@
+/*
+    Function Invocation
+
+        function declaration:
+
+            function test(){
+                //test something
+            }
+
+        function expression (gets hoisted)
+
+        var test = function(){
+            //test something
+        }
+
+    Function expression gets hoisted, function declaration does not;
+
+    //function invocation/call/execution (js engine will run it and creates an execution context)
+
+*/
